987ac4792168c0a4dab9f3d019d354d8b3ecffe4
Laevskiy <Laevskiy@mail.ru>
2023-02-28 20:55:06 +0300

    Навигация

--- cafe_menu/menu/urls.py
+++ cafe_menu/menu/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about/', about, name = 'about'),
+    path('contacts/', contacts, name = 'contacts'),
+    path('order/', order, name = 'order'),
+    path('menu/', main_menu, name = 'main_menu')
 
 ]
--- cafe_menu/menu/views.py
+++ cafe_menu/menu/views.py
@@ -1,7 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-menu = ['Главная','О нас','Меню','Контакты','Сделать заказ']
-def home (request):
+#menu = ['Главная','О нас','Меню','Контакты','Сделать заказ']
+menu = [ {'name':'Главная','url':'/'},
+         {'name':'О нас','url':'/about'},
+         {'name':'Меню'},
+         {'name':'Контакты', 'url':'/contacts'},
+         {'name':'Сделать заказ', 'url':'/order'}
 
-    return render (request, 'menu/base_menu_001.html', {'title':'Главная страница', 'menu': menu})
+]
+menu_v = [{'name':'Салат'},
+          {'name':'Супы'},
+          {'name':'Горячие блюда'},
+          {'name':'Напитки'}]
+def home (request):
+    return render (request, 'menu/main_page.html',
+                   {'title':'Главная страница', 'menu': menu,'menu_v':menu_v})
+
+def about(request):
+    return render (request,'menu/about.html',{'title':'Главное меню', 'menu': menu,'menu_v':menu_v})
+
+
+def main_menu(request):
+    return render (request,'menu/main_menu.html',{'title':'Главное меню', 'menu': menu,'menu_v':menu_v})
+
+def contacts(request):
+    return render (request,'menu/contacts.html',{'title':'Главное меню', 'menu': menu,'menu_v':menu_v})
+
+def order(request):
+    return render (request,'menu/order.html',{'title':'Главное меню', 'menu': menu,'menu_v':menu_v})
